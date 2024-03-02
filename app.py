4300688acf53ae20e10fb9c3732cd33864ea4013
joeyissa <joeyissa59@gmail.com>
2024-03-02 04:59:56 -0500

    flask app started

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import anaylze
 app = Flask(__name__)
 
 @app.route('/data', methods=['GET'])
 def get_data():
     return jsonify(anaylze.getInfo())
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
