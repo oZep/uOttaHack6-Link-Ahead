b95bd1617dbdcc70acbaead33e89c5aeb5cc0544
joeyissa <joeyissa59@gmail.com>
2024-03-02 04:52:34 -0500

    created api w flask

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import json
 from flask import Flask, jsonify, request
 import anaylze
 app = Flask(__name__)
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    return jsonify(anaylze.getInfo())
